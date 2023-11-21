8059b970ec748c793b359aac68ca94cd1b660ef3
jhongxu <jhxu2392@outlook.com>
2023-11-21 22:08:24 +0800

    feat: 1121-commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import 'virtual:uno.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'virtual:uno.css';
+// import App from './App.tsx'
+import ErrorPage from './error-page';
+import './index.css';
+
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+	},
+]);
 const root = document.getElementById('root');
 
 root &&
 	ReactDOM.createRoot(root).render(
 		<React.StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</React.StrictMode>,
 	);
